fcedaf0406099b5ed4aaa1f9bfda4810b997e4be
Frank S. Thomas <frank@timepit.eu>
2016-03-10 19:54:34 +0100

    Use the correct target directory

--- scripts/cloud-install.sh
+++ scripts/cloud-install.sh
@@ -2,8 +2,8 @@
 
 sleep 40s # give Bintray time to update the repository
 
-DFN="DEBIAN_FRONTEND=noninteractive" 
 chmod 600 cloud-identity
 ssh -q -o StrictHostKeyChecking=no -i cloud-identity $CLOUD_HOST "\
-  sudo $DFN apt-get-update-only -y --force-yes m-doc && \
-  sudo $DFN apt-get -y --force-yes install mdoc-sample-templates"
+  export DEBIAN_FRONTEND=noninteractive; \
+  sudo -E apt-get-update-only -y --force-yes m-doc && \
+  sudo -E apt-get -y --force-yes install mdoc-sample-templates"
